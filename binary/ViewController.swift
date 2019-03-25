d8e46244a34c2fad59468cb6a9114633fc5558b6
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-03-25 09:24:05 -0400

    NSAttributedString to set the placeholder with several methods

--- binary/ViewController.swift
+++ binary/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueTextField: UITextField!
+    @IBOutlet weak var binaryButton: UIButton!
+    @IBOutlet weak var decimalButton: UIButton!
+    
+    let placeholder = NSAttributedString(string: "Enter a value...", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), NSAttributedString.Key.font: UIFont(name: "Menlo", size: 36.0)!])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        valueTextField.attributedPlaceholder = placeholder
+        valueTextField.addTarget(self, action: #selector(), for: .editingChanged)
     }
-
-
+    
+    @IBAction func binaryTapped(_ sender: Any) {
+    }
+    
+    @IBAction func decimalTapped(_ sender: Any) {
+    }
+    
 }
 
